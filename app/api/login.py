ddeb083c1f51331e737f323ad8a64bdff78c5028
tsunyoku <mbruhyo@gmail.com>
2022-06-08 23:36:06 +0200

    rename "udid" param

--- app/api/login.py
+++ app/api/login.py
@@ -9,7 +9,7 @@ login_router = APIRouter(prefix="/database/accounts")
 
 @login_router.post("/loginGJAccount.php")
 async def login_request(
-    udid: str = Query(...),
+    uuid: str = Query(..., alias="udid"),
     userName: str = Query(...),
     password: str = Query(...),
     secret: Literal["Wmfv3899gc9"] = Query(...),
